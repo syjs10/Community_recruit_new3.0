8e55074f4705dcace6511737a1b1fafe1d717a3d
JS <350172898@qq.com>
2016-08-19 18:51:50 +0800

    3.0

---
+++ admin/addCate.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+      <head>
+            <meta charset="utf-8">
+            <title></title>
+      </head>
+      <body>
+            <h3>添加新生</h3>
+            <form class="" action="doAdminAction.php?act=addStudent" method="post">
+                  <table width="70%" border="1" cellpadding="5" cellspacing="0" bgcolor="#cccccc"text-algin = "center">
+                        <tr>
+                              <td>
+                                    学生名称
+                              </td>
+                              <td>
+                                    <input type="text" name="name" placeholder="姓名"/>
+                              </td>
+                        </tr>
+                        <tr>
+                              <td colspan="2">
+                                    <input type="submit" value = "添加学生"/>
+                              </td>
+                        </tr>
+                  </table>
+            </form>
+      </body>
+</html>
---
+++ new/student_commit_success.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<p>
+			请把编号交给面试官：
+		</p>
+		<?php
+			$id = $_GET['id'];
+			echo "报名编号：".$id;
+		?>
+	</body>
+</html>
